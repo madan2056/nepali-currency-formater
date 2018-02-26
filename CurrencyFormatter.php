929a793ef3ba5b20ad22cdb8ddfe7d031aec876f
Suraj Kumar Adhikari <surajprogrammer@live.com>
2018-02-26 22:14:05 +0100

    Make minor changes

--- CurrencyFormatter.php
+++ CurrencyFormatter.php
@@ -1,73 +1,70 @@
 <?php
-class CurrencyFormatter {
 
-  public function formatToMoney($number, $is_unicode = false)
+namespace NepaliCurrencyFormatter;
+
+class CurrencyFormatter
+{
+    /**
+     * Formats money and supports numbers up to 99,99,99,999
+     *
+     * @param int $number
+     * @param array $config
+     *
+     * @return mixed|string
+     */
+    public function formatToMoney(int $number, array $config = [])
     {
-        // supports upto 99,99,99,999
-        if (strlen($number) > 9)
+        if (strlen($number) > 9) {
             return $number;
-        
+        }
+
         $hundreds = $thousands = $lakhs = $crors = $negative = '';
-        if(strstr($number,"-"))
-        {
-            $number = str_replace("-","",$number);
+
+        if (strstr($number, "-")) {
+            $number = str_replace("-", "", $number);
             $negative = "-";
         }
 
-        $split_number = @explode(".",$number);
+        $split_number = @explode("." , $number);
 
         $rupee = $split_number[0];
         $paise = @$split_number[1];
 
-        if(@strlen($rupee)>3)
-        {
-            $hundreds = substr($rupee,strlen($rupee)-3);
-            $remaining = substr($rupee,0,strlen($rupee)-3);
+        if (@strlen($rupee) > 3) {
+            $hundreds = substr($rupee, strlen($rupee) - 3);
+            $remaining = substr($rupee, 0, strlen($rupee) - 3);
 
             if (strlen($remaining) < 3) {
-                $thousands = $remaining.',';
+                $thousands = $remaining . ',';
             } else {
-
                 $thousands = strrev(substr(strrev($remaining), 0, 2)).',';
                 $lakhs = strrev(substr(strrev($remaining), 2, 2)).',';
 
                 if (strlen($remaining) > 4) {
-
                     $crors = strrev(substr(strrev($remaining), 4, 2)).',';
-
                 }
-
             }
 
-            $formatted_rupee = $crors.$lakhs.$thousands.$hundreds;
-        }
-        else
-        {
+            $formatted_rupee = $crors . $lakhs . $thousands . $hundreds;
+        } else {
             $formatted_rupee = $rupee;
         }
 
-        if((int)$paise>0)
-        {
-            $formatted_paise = ".".substr($paise,0,2);
-        }
-        else
-        {
-            $formatted_paise = ".".substr("00",0,2);
-        }
+        $formatted_paise = (int) $paise > 0 
+            ? "." . substr($paise, 0, 2)
+            : "." . substr("00", 0, 2);
 
-        if($is_unicode){
-            return ' '.$this->get_nep($negative.$formatted_rupee.$formatted_paise);
-        }
-        else{
-            return ' '.$negative.$formatted_rupee.$formatted_paise;
-        }
+        return !empty($config['is_unicode']) && is_bool($config['is_unicode'])
+            ? ' ' . $this->getNepali($negative . $formatted_rupee. $formatted_paise)
+            : ' ' . $negative . $formatted_rupee . $formatted_paise;
     }
 
-    protected function get_nep($eng_str){
-        $replace 	= array("१","२","३","४","५","६","७","८","९","०" );
-        $find 	 	= array("1","2","3","4","5","6","7","8","9","0" );
-        $nep_str	=  str_replace($find, $replace, $eng_str);
-        return $nep_str;
+    protected function getNepali(string $text): string
+    {
+        return str_replace(
+            ["1","2","3","4","5","6","7","8","9","0"],
+            ["१","२","३","४","५","६","७","८","९","०"],
+            $text
+        );
     }
-
 }
--- README.md
+++ README.md
@@ -2,14 +2,12 @@
 
 # For getting formated currency in english 
 ```php
-   $obj = new CurrencyFormatter();
-   $obj->formatToMoney(520250);
+(new NepaliCurrencyFormatter\CurrencyFormatter())->formatToMoney(520250);
 ```
 -- will output: 5,20,250
 
 # For getting formated currency in nepali
 ```php
-   $obj = new CurrencyFormatter();
-   $obj->formatToMoney(520250, true);
+(new NepaliCurrencyFormatter\CurrencyFormatter())->formatToMoney(520250, ['is_unicode' => true]);
 ```
 -- will output: ५,२०,२५०
